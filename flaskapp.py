0efb908e264a04aa8cc3e3fdbb862a4def1f8b0e
scrc <rakeshhotker@gmail.com>
2022-09-01 17:33:48 +0530

    showcurrentstatus

---
+++ flaskapp.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for
+from flask import request
+import requests
+
+app = Flask(__name__, static_url_path='/static')
+
+x=""
+@app.route("/", methods=["POST", "GET"])
+def home():
+
+    return render_template('app2.html')
+
+
+@app.route("/switch", methods=["POST", "GET"])
+def switch():
+    command = request.args.get("command")
+    id = request.args.get("id")
+    print(command,id)
+    url = "http://dev-onem2m.iiit.ac.in:443/~/in-cse/in-name/AE-WiSUN/Data"
+    headers = {
+        "X-M2M-Origin": "devtest:devtest",
+        "Content-type": "application/json;ty=4"
+    }
+    on={
+        "m2m:cin": {
+                    "con":'.'+command+id,
+                    "lbl":"",
+                    "cnf":"text"
+        }
+    }
+    off = {
+        "m2m:cin": {
+            "con":'.'+command+id,
+            "lbl":"",
+            "cnf":"text"
+        }
+    }
+    test={}
+    if (command == "on"):
+        x=requests.post(url,headers=headers,json=on)
+        test=x.text
+        print(x.text)
+    if(command=="off"):
+        x=requests.post(url,headers=headers,json=off)
+        test=x.text
+        print(x.text)
+    return test
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
+    # app.run()
